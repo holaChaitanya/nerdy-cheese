8d45d4c47b81e9ca47691be5fa622f1e89887159
Chaitanya Aaditya <ca.chaitanya.2204@gmail.com>
2024-04-11 13:16:55 +0530

    escape unused fn arg

--- src/main/main.ts
+++ src/main/main.ts
@@ -37,7 +37,7 @@ ipcMain.on('ipc-example', async (event, arg) => {
 ipcMain.on('electron-store-get', async (event, val) => {
   event.returnValue = store.get(val);
 });
-ipcMain.on('electron-store-set', async (event, key, val) => {
+ipcMain.on('electron-store-set', async (_, key, val) => {
   store.set(key, val);
 });
 
