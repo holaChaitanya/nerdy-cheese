e0a36adf118d26b1cef356da85a99e29396e89e9
Chaitanya Aaditya <ca.chaitanya.2204@gmail.com>
2024-05-05 18:13:18 +0530

    start timer automatically

--- src/main/main.ts
+++ src/main/main.ts
@@ -411,6 +411,9 @@ app
   .then(() => {
     // createWindow();
     createTray();
+    if (store.get('start_timer')) {
+      startSession({});
+    }
     app.on('activate', () => {
       // On macOS it's common to re-create a window in the app when the
       // dock icon is clicked and there are no other windows open.
