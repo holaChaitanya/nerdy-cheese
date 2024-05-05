1ad238bb8208f8f57ae8323244f4acc565c1bb59
Chaitanya Aaditya <ca.chaitanya.2204@gmail.com>
2024-05-05 08:53:01 +0530

    chore refactor

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,52 +1,13 @@
-import { useState, useEffect } from 'react';
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
+import Break from './Break';
 
 import './App.css';
-import { DEFAULT_BREAK_DURATION } from '../main/constants';
-
-function Hello() {
-  const [seconds, setSeconds] = useState(DEFAULT_BREAK_DURATION);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setSeconds((prev) => prev - 1);
-    }, 1000);
-
-    return () => clearInterval(interval);
-  }, []);
-
-  useEffect(() => {
-    if (seconds <= 0) {
-      window.electron.ipcRenderer.sendMessage('start-session');
-      window.close();
-    }
-  }, [seconds]);
-
-  return (
-    <div>
-      <h2>{seconds > 0 && <p>Timer: {seconds} seconds</p>}</h2>
-      <h1>Your eyes need rest :)</h1>
-      <div className="Hello">
-        World looks more beautiful when your eyes are healthy
-      </div>
-      <button
-        type="button"
-        onClick={() => {
-          window.electron.ipcRenderer.sendMessage('start-session');
-          window.close();
-        }}
-      >
-        Skip the break
-      </button>
-    </div>
-  );
-}
 
 export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Hello />} />
+        <Route path="/" element={<Break />} />
       </Routes>
     </Router>
   );
