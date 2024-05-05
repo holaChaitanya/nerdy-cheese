07ea4f183c491fd0e317a1a2a6727b2daf16d352
Chaitanya Aaditya <ca.chaitanya.2204@gmail.com>
2024-05-05 15:21:47 +0530

    changes

--- src/renderer/Settings.tsx
+++ src/renderer/Settings.tsx
@@ -1,4 +1,56 @@
-import { Button, ConfigProvider, theme } from 'antd';
+import { ConfigProvider, theme, Layout, Menu } from 'antd';
+
+const { Sider, Footer, Content, Header } = Layout;
+
+const SideBarItems = [
+  {
+    key: '1',
+    // icon: <MailOutlined />,
+    label: 'Navigation One',
+  },
+  {
+    key: '2',
+    // icon: <CalendarOutlined />,
+    label: 'Navigation Two',
+  },
+  {
+    key: 'sub1',
+    label: 'Navigation Two',
+    // icon: <AppstoreOutlined />,
+    children: [
+      { key: '3', label: 'Option 3' },
+      { key: '4', label: 'Option 4' },
+      {
+        key: 'sub1-2',
+        label: 'Submenu',
+        children: [
+          { key: '5', label: 'Option 5' },
+          { key: '6', label: 'Option 6' },
+        ],
+      },
+    ],
+  },
+  {
+    key: 'sub2',
+    label: 'Navigation Three',
+    // icon: <SettingOutlined />,
+    children: [
+      { key: '7', label: 'Option 7' },
+      { key: '8', label: 'Option 8' },
+      { key: '9', label: 'Option 9' },
+      { key: '10', label: 'Option 10' },
+    ],
+  },
+  {
+    key: 'link',
+    // icon: <LinkOutlined />,
+    label: (
+      <a href="https://ant.design" target="_blank" rel="noopener noreferrer">
+        Ant Design
+      </a>
+    ),
+  },
+];
 
 function Settings() {
   return (
@@ -14,8 +66,31 @@ function Settings() {
         algorithm: theme.darkAlgorithm,
       }}
     >
-      <p>This is Setting</p>
-      <Button onClick={() => {}}>click me </Button>
+      <Layout style={{ minHeight: '100vh' }}>
+        <Sider>
+          <Menu
+            defaultSelectedKeys={['1']}
+            mode="vertical"
+            items={SideBarItems}
+          />
+        </Sider>
+        <Layout>
+          <Header style={{ padding: 0 }} />
+          <Content style={{ margin: '0 16px' }}>
+            <div
+              style={{
+                padding: 24,
+                minHeight: 360,
+              }}
+            >
+              Bill is a cat.
+            </div>
+          </Content>
+          <Footer style={{ textAlign: 'center' }}>
+            Ant Design ©{new Date().getFullYear()} Created by Ant UED
+          </Footer>
+        </Layout>
+      </Layout>
     </ConfigProvider>
   );
 }
