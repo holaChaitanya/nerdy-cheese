e413a9f7c8e9286808d7f6a8f84eb082eee93cc0
Chaitanya Aaditya <ca.chaitanya.2204@gmail.com>
2024-05-05 13:53:42 +0530

    add mui

--- src/renderer/Settings.tsx
+++ src/renderer/Settings.tsx
@@ -1,5 +1,17 @@
+import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
+
 function Settings() {
-  return <p>This is Setting</p>;
+  return (
+    <div>
+      <p>This is Setting</p>
+      <Stack spacing={2} direction="row">
+        <Button variant="text">Text</Button>
+        <Button variant="contained">Contained</Button>
+        <Button variant="outlined">Outlined</Button>
+      </Stack>
+    </div>
+  );
 }
 
 export default Settings;
