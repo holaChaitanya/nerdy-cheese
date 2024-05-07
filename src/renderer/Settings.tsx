0889d19ceb95734b5971aef5d2380dbf1d33c5c6
Chaitanya Aaditya <ca.chaitanya.2204@gmail.com>
2024-05-07 15:31:47 +0530

    adds tailwind

--- src/renderer/Settings.tsx
+++ src/renderer/Settings.tsx
@@ -1,5 +1,9 @@
 function Settings() {
-  return <p>This is Setting</p>;
+  return (
+    <div>
+      <h1 className="text-2xl font-bold underline">This is Setting</h1>
+    </div>
+  );
 }
 
 export default Settings;
