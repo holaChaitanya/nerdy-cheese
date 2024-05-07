bfb5f5d1d785744d5a1f12c64c3530c77656c5bc
Chaitanya Aaditya <ca.chaitanya.2204@gmail.com>
2024-05-07 16:37:55 +0530

    add tailwind button

--- src/renderer/Settings.tsx
+++ src/renderer/Settings.tsx
@@ -1,7 +1,10 @@
+import { Button } from './components/ui/button';
+
 function Settings() {
   return (
     <div>
       <h1 className="text-2xl font-bold underline">This is Setting</h1>
+      <Button>Button</Button>
     </div>
   );
 }
