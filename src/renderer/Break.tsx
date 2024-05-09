65fe48114fe64b320fc14dee23c6a7e3e1d93752
Chaitanya Aaditya <ca.chaitanya.2204@gmail.com>
2024-05-09 08:21:12 +0530

    add aurora bg

--- src/renderer/Break.tsx
+++ src/renderer/Break.tsx
@@ -1,5 +1,7 @@
 import { useState, useEffect } from 'react';
+import { motion } from 'framer-motion';
 import { DEFAULT_BREAK_DURATION } from '../main/constants';
+import { AuroraBackground } from './components/ui/aurora-background';
 
 function Break() {
   const [seconds, setSeconds] = useState(DEFAULT_BREAK_DURATION);
@@ -20,22 +22,46 @@ function Break() {
   }, [seconds]);
 
   return (
-    <div>
-      <h2>{seconds > 0 && <p>Timer: {seconds} seconds</p>}</h2>
-      <h1>Your eyes need rest :)</h1>
-      <div className="Hello">
-        World looks more beautiful when your eyes are healthy
-      </div>
-      <button
-        type="button"
-        onClick={() => {
-          window.electron.ipcRenderer.sendMessage('start-session');
-          window.close();
+    <AuroraBackground>
+      <motion.div
+        initial={{ opacity: 0.0, y: 40 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{
+          delay: 0.3,
+          duration: 0.8,
+          ease: 'easeInOut',
         }}
+        className="relative flex flex-col gap-4 items-center justify-center px-4"
       >
-        Skip the break
-      </button>
-    </div>
+        {/* <h2>{seconds > 0 && <p>Timer: {seconds} seconds</p>}</h2>
+        <h1>Your eyes need rest :)</h1>
+        <div className="Hello">
+          World looks more beautiful when your eyes are healthy
+        </div>
+        <button
+          type="button"
+          onClick={() => {
+            window.electron.ipcRenderer.sendMessage('start-session');
+            window.close();
+          }}
+        >
+          Skip the break
+        </button> */}
+
+        <div className="text-3xl md:text-7xl font-bold dark:text-white text-center">
+          Background lights are cool you know.
+        </div>
+        <div className="font-extralight text-base md:text-4xl dark:text-neutral-200 py-4">
+          And this, is chemical burn.
+        </div>
+        <button
+          type="button"
+          className="bg-black dark:bg-white rounded-full w-fit text-white dark:text-black px-4 py-2"
+        >
+          Debug now
+        </button>
+      </motion.div>
+    </AuroraBackground>
   );
 }
 
