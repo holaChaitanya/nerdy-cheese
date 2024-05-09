bd139e8f94ece148504d7c7e50ded3e0917e9334
Chaitanya Aaditya <ca.chaitanya.2204@gmail.com>
2024-05-09 09:19:25 +0530

    bindings for startupsettings

--- src/renderer/startupSettings.tsx
+++ src/renderer/startupSettings.tsx
@@ -1,10 +1,17 @@
 /* eslint-disable import/prefer-default-export */
 
+import { useState } from 'react';
 import { Card, CardContent, CardHeader, CardTitle } from './components/ui/card';
 import { Label } from './components/ui/label';
 import { Switch } from './components/ui/switch';
 
 export function StartupSettings() {
+  const launchAtLoginInStore = window.electron.store.get('launch_at_login');
+  const startTimerInStore = window.electron.store.get('start_timer');
+
+  const [launchAtLogin, setLaunchAtLogin] = useState(launchAtLoginInStore);
+  const [startTimer, setStartTimer] = useState(startTimerInStore);
+
   return (
     <Card>
       <CardHeader>
@@ -12,16 +19,32 @@ export function StartupSettings() {
       </CardHeader>
       <CardContent className="grid gap-6">
         <div className="flex items-center justify-between space-x-2">
-          <Label htmlFor="necessary" className="flex flex-col space-y-1">
+          <Label htmlFor="launch_at_login" className="flex flex-col space-y-1">
             <span>Launch at login</span>
           </Label>
-          <Switch id="necessary" defaultChecked />
+          <Switch
+            id="launch_at_login"
+            key="launch_at_login"
+            checked={launchAtLogin}
+            onCheckedChange={(checked) => {
+              setLaunchAtLogin(checked);
+              window.electron.store.set('launch_at_login', checked);
+            }}
+          />
         </div>
         <div className="flex items-center justify-between space-x-2">
-          <Label htmlFor="functional" className="flex flex-col space-y-1">
+          <Label htmlFor="start_timer" className="flex flex-col space-y-1">
             <span>Start timer automatically on launch</span>
           </Label>
-          <Switch id="functional" />
+          <Switch
+            id="start_timer"
+            key="start_timer"
+            checked={startTimer}
+            onCheckedChange={(checked) => {
+              setStartTimer(checked);
+              window.electron.store.set('start_timer', checked);
+            }}
+          />
         </div>
       </CardContent>
     </Card>
