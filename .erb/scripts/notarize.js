703b7fe4fbcc8ba13ff9d1cfcca36e1adfdce579
Chaitanya Aaditya <ca.chaitanya.2204@gmail.com>
2024-05-27 09:40:10 +0530

    changes

--- .erb/scripts/notarize.js
+++ .erb/scripts/notarize.js
@@ -1,8 +1,8 @@
 const { notarize } = require('@electron/notarize');
-// const { build } = require('../../package.json');
+const { build } = require('../../package.json');
 
 exports.default = async function notarizeMacos(context) {
-  const { electronPlatformName } = context;
+  const { electronPlatformName, appOutDir } = context;
   if (electronPlatformName !== 'darwin') {
     return;
   }
@@ -21,14 +21,14 @@ exports.default = async function notarizeMacos(context) {
     return;
   }
 
-  // const appName = context.packager.appInfo.productFilename;
+  const appName = context.packager.appInfo.productFilename;
 
   await notarize({
     tool: 'notarytool',
-    // appBundleId: build.appId,
-    // appPath: `${appOutDir}/${appName}.app`,
-    // appleId: process.env.APPLE_ID,
-    // appleIdPassword: process.env.APPLE_APP_SPECIFIC_PASSWORD,
+    appBundleId: build.appId,
+    appPath: `${appOutDir}/${appName}.app`,
+    appleId: process.env.APPLE_ID,
+    appleIdPassword: process.env.APPLE_APP_SPECIFIC_PASSWORD,
     teamId: process.env.APPLE_TEAM_ID,
   });
 };
