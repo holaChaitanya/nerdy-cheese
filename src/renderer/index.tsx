25b3f4c3b2b0857126e0e01d481ca1cb7cbbf6a2
Chaitanya Aaditya <ca.chaitanya.2204@gmail.com>
2024-06-18 20:27:35 +0530

    add sentry

--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -1,6 +1,28 @@
 import { createRoot } from 'react-dom/client';
 import mixpanel from 'mixpanel-browser';
+import * as Sentry from '@sentry/electron/renderer';
+import { init as reactInit } from '@sentry/react';
 import App from './App';
+
+Sentry.init(
+  {
+    integrations: [
+      Sentry.browserTracingIntegration(),
+      Sentry.replayIntegration(),
+    ],
+
+    // Set tracesSampleRate to 1.0 to capture 100%
+    // of transactions for performance monitoring.
+    // We recommend adjusting this value in production
+    tracesSampleRate: 1.0,
+
+    // Capture Replay for 10% of all sessions,
+    // plus for 100% of sessions with an error
+    replaysSessionSampleRate: 0.1,
+    replaysOnErrorSampleRate: 1.0,
+  },
+  reactInit,
+);
 
 mixpanel.init('fb10e8ad642b8be1d8f8f57f13b28d92', {
   debug: true,
